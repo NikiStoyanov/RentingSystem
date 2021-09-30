5b9ae6d781acc13fc3efa10efdddd7f77f680247
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2021-09-30 11:28:23 +0300

    Create models, migration and config the database connection.

---
+++ app/Models/Room.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['size'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function desks() {
+        return $this->hasMany('App\Desk');
+    }
+}
